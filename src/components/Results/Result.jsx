9a15bd14f05a834231c0de9fafb335ac9321095f
Alex <58560420+AlexMA2@users.noreply.github.com>
2022-05-09 23:24:47 -0500

    A

--- src/components/Results/Result.jsx
+++ src/components/Results/Result.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import { useEffect } from "react";
 
-const Result = ({ results }) => {
+import "./Result.css";
+
+const Result = ({ result }) => {
+  const [emotions, setEmotions] = React.useState({});
+
+  useEffect(() => {
+    if (result) {
+      setEmotions(result?.emotion.document.emotion);
+    }
+  }, [result]);
+
   return (
-    <div>
+    <div className="result">
       <h2> Resultados </h2>
-      <ul></ul>
+      <table>
+        <thead>
+          <tr>
+            <th> Emoción </th>
+            <th> Porcentaje </th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(emotions).map((emotion) => (
+            <tr key={emotion}>
+              <td> {emotion} </td>
+              <td> {emotions[emotion] * 100}% </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
